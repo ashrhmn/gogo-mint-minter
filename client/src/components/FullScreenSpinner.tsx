f9e5e8fa7e10b0cb8c6aeae027cc7a98878f5da3
ashrhmn <ashrhmn99@gmail.com>
2023-02-12 20:15:22 +0600

    add bigger loader

--- client/src/components/FullScreenSpinner.tsx
+++ client/src/components/FullScreenSpinner.tsx
@@ -3,7 +3,7 @@ import Spinner from "./Spinner";
 
 const FullScreenSpinner = () => {
   return (
-    <div className="fixed inset-0 z-50 w-full h-[100vh] flex justify-center items-center">
+    <div className="fixed inset-0 z-50 w-full h-[100vh] flex justify-center items-center scale-150">
       <Spinner />
     </div>
   );
