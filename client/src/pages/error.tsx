c87aa6862a75f7e0ee7e35c27af0d3a4a033695b
Ashik Rahman <ashrhmn99@gmail.com>
2023-05-23 12:47:38 +0600

    update 404 page

--- client/src/pages/error.tsx
+++ client/src/pages/error.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useRouteError } from "react-router-dom";
 
 const ErrorPage = ({ msg }: { msg?: string }) => {
   const error: any = useRouteError();
   console.error(error);
+  const errorText = useMemo(() => {
+    if (error?.status === 404) {
+      return "The page you are requesting could not be found.";
+    }
+    return "Sorry, an unexpected error has occurred.";
+  }, [error]);
   return (
     <div className="flex h-[100vh] flex-col justify-center items-center">
       <h1 className="text-7xl">Oops!</h1>
-      <p className="text-4xl my-4">Sorry, an unexpected error has occurred.</p>
+      <p className="text-4xl my-4">{errorText}</p>
       <p>
         <i>{error?.statusText || error?.message || msg}</i>
       </p>
